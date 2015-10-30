6a3e28ddc7654a22a3e2c49ef8c8e6db721c5b77
joomanba <joomanba@gmail.com>
2015-10-30 15:42:07 +0900

    짝수번째 숫자는 x2, 홀수번째 숫자는 x1 하는 f(int num) 메소드 추가

--- LUHN/src/Luhn.java
+++ LUHN/src/Luhn.java
@@ -1,23 +1,21 @@
 
 public class Luhn {
 	
-	
-	
-	public static void main(String args[]){
-		
-		System.out.println("Hello World!");
-		Luhn luhn = new Luhn();
-		luhn.valid("44321");
-	}
-
 	public boolean valid(String num) {
-		return (dat(num, 0) + 2*dat(num, 1) + dat(num, 2) + 2*dat(num, 3) + dat(num, 4)) % 10 == 0;
+		return (f(1)*digitAt(num, 1) + f(2)*digitAt(num, 2) + f(3)*digitAt(num, 3) + 
+				f(4)*digitAt(num, 4) + f(5)*digitAt(num, 5)) % 10 == 0;
 	}
 
-	private int dat(String num, int index) {
-		return new Integer((num.substring(index, index + 1))).intValue();
+	private int digitAt(String num, int index) {
+		return new Integer((num.substring(index -1, index))).intValue();
 	} 
 	
+	private int f(int i) {
+		if ((i % 2) == 0)
+			return 2;
+		else
+			return 1;
+	}
 	
 }
 
