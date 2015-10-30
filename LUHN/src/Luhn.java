344e63cd4c6fca94bdccdec58f199f861f864fc9
joomanba <joomanba@gmail.com>
2015-10-30 10:58:17 +0900

    자릿수값에 따라 카드번호를 가져오는 dat 메소드 만

--- LUHN/src/Luhn.java
+++ LUHN/src/Luhn.java
@@ -1,13 +1,23 @@
 
 public class Luhn {
 	
+	
+	
 	public static void main(String args[]){
+		
 		System.out.println("Hello World!");
+		Luhn luhn = new Luhn();
+		luhn.valid("44321");
 	}
 
 	public boolean valid(String num) {
-		return (4 + 2*4 + 3 + 2*2 + 1) % 10 == 0;
+		return (dat(num, 0) + 2*dat(num, 1) + dat(num, 2) + 2*dat(num, 3) + dat(num, 4)) % 10 == 0;
 	}
+
+	private int dat(String num, int index) {
+		return new Integer((num.substring(index, index + 1))).intValue();
+	} 
+	
 	
 }
 
