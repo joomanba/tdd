bcbe8db9250086bbf0fc923974bb647838730618
joomanba <joomanba@gmail.com>
2015-10-30 10:16:26 +0900

    valid 메소드 생

--- LUHN/src/Luhn.java
+++ LUHN/src/Luhn.java
@@ -1,11 +1,15 @@
 
 public class Luhn {
+	
 	public static void main(String args[]){
 		System.out.println("Hello World!");
 	}
 
 	public boolean valid(String num) {
-		return true;
+		return (4 + 2*4 + 3 + 2*2 + 1) % 10 == 0;
 	}
 	
 }
+
+
+
